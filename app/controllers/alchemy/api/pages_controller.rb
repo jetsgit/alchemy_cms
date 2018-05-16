95a8521c89f1ece295e0b753e282d88d9cee97ef
Jerrold Thompson <jet@whidbey.com>
2018-05-15 19:06:02 -0700

    add Api::PagesController#nested

--- app/controllers/alchemy/api/pages_controller.rb
+++ app/controllers/alchemy/api/pages_controller.rb
@@ -21,6 +21,18 @@ module Alchemy
       respond_with @page
     end
 
+    # Below experimental, cherry-pick by jet from 0380f2e  committed on Oct 5, 2016
+    # TODO test
+    def nested
+      @page = Page.find_by(id: params[:page_id]) || Language.current_root_page
+
+      render json: PageTreeSerializer.new(@page,
+        ability: current_ability,
+        user: current_alchemy_user,
+        elements: params[:elements],
+        full: true)
+    end
+
     private
 
     def load_page
